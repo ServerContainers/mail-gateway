45581ecb14c4c765b928535e4bb19b6715ae95e0
MarvAmBass <marvambass@gmail.com>
2017-07-22 11:02:24 +0200

    rsyslog finish script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -232,6 +232,7 @@ EOF
   echo ">> RUNIT - create services"
   mkdir -p /etc/sv/rsyslog /etc/sv/postfix /etc/sv/amavis /etc/sv/clamd /etc/sv/freshclam
   echo -e '#!/bin/sh\nexec /usr/sbin/rsyslogd -n' > /etc/sv/rsyslog/run
+    echo -e '#!/bin/sh\nrm /var/run/rsyslogd.pid' > /etc/sv/rsyslog/finish
   echo -e '#!/bin/sh\nexec /usr/sbin/amavisd-new foreground' > /etc/sv/amavis/run
   echo -e '#!/bin/sh\nservice postfix start; while ps aux | grep [p]ostfix | grep [m]aster > /dev/null 2> /dev/null; do sleep 5; done' > /etc/sv/postfix/run
     echo -e '#!/bin/sh\nservice postfix stop' > /etc/sv/postfix/finish
