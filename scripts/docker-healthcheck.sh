64263c35a5e08112ac888b5a97c35056c3a07bda
DrDoug88 <DrDoug88@outlook.com>
2022-10-08 09:48:29 -0500

    Updated healthcheck

--- scripts/docker-healthcheck.sh
+++ scripts/docker-healthcheck.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Check for changes to certificates, reload postfix if different.
+diff /etc/postfix/tls /tmp/tls
+if [ $? -ne 0 ]; then
+  postfix reload
+  rm -rf /tmp/tls 2> /dev/null
+  cp -a /etc/postfix/tls /tmp/tls
+fi
+
 [[ $(ps aux | grep '[r]unsvdir\|[r]syslogd\|[s]bin/master' | wc -l) -ge '3' ]]
 exit $?
